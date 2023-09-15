bbda3513b3ce61fb6506758885c8e3e366661f3a
Conor Hynes <conor@managexr.com>
2023-09-14 17:24:12 -0700

    Typo

--- Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
+++ Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
@@ -130,7 +130,7 @@ namespace MXR.SDK {
         public bool AreControllerSettingsHidden => TryGet(x => x.customLauncherSettings.hiddenSettings.controller, false);
 
         /// <summary>
-        /// Helper property to checker whether the feature flag HIDE_ALL_CATEGORY_OF_LIBRARY is enabled 
+        /// Helper property to check whether the feature flag HIDE_ALL_CATEGORY_OF_LIBRARY is enabled 
         /// </summary>
         [JsonIgnore]
         public bool HideAllCategory => featureFlags.Contains("HIDE_ALL_CATEGORY_OF_LIBRARY");
