c116573ea9590ca9c1e06fccc0ce333e9dc6e599
Conor <conor@managexr.com>
2024-08-15 15:00:28 -0700

    Added Style field

--- Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
+++ Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
@@ -379,6 +379,14 @@ namespace MXR.SDK {
         RIGHT
     }
 
+
+    [Serializable]
+    public enum Style {
+        PADDING,
+        NO_PADDING,
+        NO_BACKGROUND
+    }
+
     /// <summary>
     /// Customization settings for the content cards shown in the library
     /// </summary>
@@ -393,6 +401,13 @@ namespace MXR.SDK {
         /// Whether the content cards should show the content type text ("App", "Video", WebXR")
         /// </summary>
         public bool showContentType = true;
+
+        /// <summary>
+        /// The style of the content cards
+        /// </summary>
+        public Style style;
+
+
     }
 
     /// <summary>
