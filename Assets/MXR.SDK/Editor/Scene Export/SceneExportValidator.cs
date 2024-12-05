0039e5ebe426ce0b2a907751c6aec39defdbccea
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2024-12-06 03:49:18 +0530

    Better custom script detection. Rename Violation to SceneExportViolation
    Previously the validator checked if any scripts were in a namespace other than TMPro.* and UnityEngine.*
    
    Since custom scripts could also be placed in the same namespace, that check wasn't as robust.
    
    Now the validator checks the name of the DLL where a script is located. Currently the supported DLLs are Unity.TextMeshPro, UnityEngine.UI and Unity.RenderPipelines.Universal.Runtime
    
    More may be added over time.
    ---
    Added some comments to SceneExportValidation.cs

--- Assets/MXR.SDK/Editor/Scene Export/SceneExportValidator.cs
+++ Assets/MXR.SDK/Editor/Scene Export/SceneExportValidator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using System.IO;
 
 using UnityEditor;
 using UnityEngine;
@@ -12,8 +13,8 @@ namespace MXR.SDK.Editor {
         /// <summary>
         /// Returns a list of violations in the active scene
         /// </summary>
-        public List<Violation> Validate() {
-            var violations = new List<Violation>();
+        public List<SceneExportViolation> Validate() {
+            var violations = new List<SceneExportViolation>();
 
             var renderPipelineViolation = GetRenderPipelineViolation();
             if (renderPipelineViolation != null)
@@ -28,12 +29,16 @@ namespace MXR.SDK.Editor {
             return violations;
         }
 
-        private Violation GetRenderPipelineViolation() {
+
+        /// <summary>
+        /// Checks and ensures the project not configured to use a render pipeline other than Universal Render Pipeline
+        /// </summary>
+        private SceneExportViolation GetRenderPipelineViolation() {
             var renderPipelineAsset = GraphicsSettings.defaultRenderPipeline;
-            var violation = new Violation(
-                Violation.Types.UnsupportedRenderPipeline, 
-                false, 
-                "Only Universal Render Pipeline is supported.", 
+            var violation = new SceneExportViolation(
+                SceneExportViolation.Types.UnsupportedRenderPipeline,
+                false,
+                "Only Universal Render Pipeline is supported.",
                 null
             );
             if (renderPipelineAsset != null && renderPipelineAsset.GetType().Name == "UniversalRenderPipelineAsset")
@@ -42,63 +47,103 @@ namespace MXR.SDK.Editor {
                 return violation;
         }
 
-        private List<Violation> GetShaderViolations() {
+        /// <summary>
+        /// Checks and ensures the scene doesn't have materials that use unsupported shaders.
+        /// Only shaders in the following namespaces/family are supported:
+        /// - Universal Render Pipeline
+        /// - Unlit
+        /// - UI
+        /// - Sprites
+        /// - Skybox
+        /// </summary>
+        private List<SceneExportViolation> GetShaderViolations() {
             var dependencies = AssetDatabase.GetDependencies(new string[] {
                 SceneManager.GetActiveScene().path
             });
             var unsupportedMaterials = dependencies
                 .Where(x => x.EndsWith(".mat"))
                 .Select(x => AssetDatabase.LoadAssetAtPath<Material>(x))
-                .Where(x => !x.shader.name.Equals("Hidden/"))
                 .Where(x => !x.shader.name.StartsWith("Universal Render Pipeline/"))
                 .Where(x => !x.shader.name.StartsWith("Unlit/"))
                 .Where(x => !x.shader.name.StartsWith("UI/"))
                 .Where(x => !x.shader.name.StartsWith("Sprites/"))
                 .Where(x => !x.shader.name.StartsWith("Skybox/"));
-            return unsupportedMaterials.Select(x => new Violation(
-                Violation.Types.UnsupportedShader,
+            return unsupportedMaterials.Select(x => new SceneExportViolation(
+                SceneExportViolation.Types.UnsupportedShader,
                 false,
                 "Only default URP, Unlit, UI, Sprites and Skybox shaders are supported.",
                 x)).ToList();
         }
 
-        private List<Violation> GetScriptViolations() {
-            var components = Object.FindObjectsOfType<MonoBehaviour>()
-                    .Where(x => !x.GetType().FullName.StartsWith("TMPro"))
-                    .Where(x => !x.GetType().FullName.StartsWith("UnityEngine"));
-            return components.Select(x => new Violation(
-                Violation.Types.CustomScriptFound,
+        /// <summary>
+        /// Checks and ensures the scene doesn't use any custom scripts. 
+        /// Only scripts in the following DLLs are supported:
+        /// Unity.TextMeshPro.dll
+        /// UnityEngine.UI.dll
+        /// Unity.RenderPipelines.Universal.Runtime.dll
+        /// </summary>
+        /// <returns></returns>
+        private List<SceneExportViolation> GetScriptViolations() {
+            var supportedDLLs = new string[]{
+                "Unity.TextMeshPro.dll",
+                "UnityEngine.UI.dll",
+                "Unity.RenderPipelines.Universal.Runtime.dll"
+            };
+            var unsupportedComponents = Object.FindObjectsOfType<MonoBehaviour>()
+                .Where(x => {
+                    var codeBasePath = x.GetType().Assembly.GetName().CodeBase;
+                    var codeBaseFileName = Path.GetFileName(codeBasePath);
+                    return !supportedDLLs.Contains(codeBaseFileName);
+                });
+            return unsupportedComponents.Select(x => new SceneExportViolation(
+                SceneExportViolation.Types.CustomScriptFound,
                 false,
-                "Custom scripts and components are not supported. Please remove them from the scene.",
+                "Custom scripts/components are not supported. Please remove them from the scene.",
                 x
             )).ToList();
         }
 
-        private List<Violation> GetCameraViolations() {
+        /// <summary>
+        /// Checks and ensures the scene doesn't have a camera.
+        /// </summary>
+        /// <returns></returns>
+        private List<SceneExportViolation> GetCameraViolations() {
             var cameras = Object.FindObjectsOfType<Camera>();
-            return cameras.Select(x => new Violation(
-                Violation.Types.CameraFound,
+            return cameras.Select(x => new SceneExportViolation(
+                SceneExportViolation.Types.CameraFound,
                 false,
                 "Scene cameras are not supported. Please remove cameras from the scene.",
                 x
             )).ToList();
         }
 
-        private List<Violation> GetLightViolations() {
+        /// <summary>
+        /// Checks and warns if there are realtime or mixed lights in the scene.
+        /// These violations are a warning, not errors that prevents export.
+        /// </summary>
+        /// <returns></returns>
+        private List<SceneExportViolation> GetLightViolations() {
             var nonBakedLights = Object.FindObjectsOfType<Light>()
                 .Where(x => x.lightmapBakeType != LightmapBakeType.Baked).ToList();
-            return nonBakedLights.Select(x => new Violation(
-                Violation.Types.NonBakedLight,
+            return nonBakedLights.Select(x => new SceneExportViolation(
+                SceneExportViolation.Types.NonBakedLight,
                 true,
-                "Realtime and Mixed lights are not recommended. Consider lightmapping your scene with baked lights.",
+                "Realtime and Mixed lights are not recommended. " +
+                "Consider lightmapping your scene with baked lights " +
+                "and only using realtime lights if you truly need them" +
+                "as they can impact performance.",
                 x
             )).ToList();
         }
 
-        private List<Violation> GetEventSystemViolations() {
+        /// <summary>
+        /// Checks and ensures the scene doesn't have an EventSystem
+        /// </summary>
+        /// <returns></returns>
+        private List<SceneExportViolation> GetEventSystemViolations() {
             var eventSystems = Object.FindObjectsOfType<EventSystem>();
-            return eventSystems.Select(x => new Violation(
-                Violation.Types.EventSystemFound,
+            return eventSystems.Select(x => new SceneExportViolation(
+                SceneExportViolation.Types.EventSystemFound,
                 false,
                 "There cannot be an EventSystem on the scene. Please remove them from the scene.",
                 x
