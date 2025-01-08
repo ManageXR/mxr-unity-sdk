bbaf1e6a61f5f22bed98d0196d81cf0a3c8e3f47
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2025-01-09 04:25:29 +0530

    Added some more DeviceData fields

--- Assets/MXR.SDK/Runtime/Types/DeviceTypes.cs
+++ Assets/MXR.SDK/Runtime/Types/DeviceTypes.cs
@@ -3,6 +3,11 @@
 namespace MXR.SDK {
     [Serializable]
     public class DeviceData {
+        public long availableStorage;
+        public long totalStorage;
         public bool isQfbDevice;
+        public string firmwareVersion;
+        public string model;
+        public string manufacturer;
     }
 }
