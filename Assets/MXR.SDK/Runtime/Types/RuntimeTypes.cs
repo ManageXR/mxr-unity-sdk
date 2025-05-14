4ed197cf48d2309f93077d5a07b0e4aabd6d6768
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2025-05-14 06:22:49 +0530

    Move environmentFile to backgroundSettings
    See linked task for why and more details.

--- Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
+++ Assets/MXR.SDK/Runtime/Types/RuntimeTypes.cs
@@ -316,11 +316,6 @@ namespace MXR.SDK {
         /// The theme to be used for the homescreen
         /// </summary>
         public CustomHomeScreenColors colors = new CustomHomeScreenColors();
-
-        /// <summary>
-        /// The environment deployed with the configuration to be used in the homescreen, if any.
-        /// </summary>
-        public EnvironmentFile environmentFile;
     }
 
     /// <summary>
@@ -403,6 +398,11 @@ namespace MXR.SDK {
     /// </summary>
     [Serializable]
     public class BackgroundSettings {
+        /// <summary>
+        /// The environment deployed with the configuration to be used in the homescreen, if any.
+        /// </summary>
+        public EnvironmentFile environmentFile;
+
         /// <summary>
         /// Whether the user should be forced into passthrough mode.
         /// </summary>
